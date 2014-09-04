16ef3187310b47bceec5a234c72723941d286aaf
ltfschoen <ltfschoen@gmail.com>
2014-09-04 15:10:33 +1000

    Changed Turbolinks in Asset Pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require underscore
 //= require bootstrap
 //= require modernizr-latest-2.8.3
+//= require turbolinks
 //= require_tree .
