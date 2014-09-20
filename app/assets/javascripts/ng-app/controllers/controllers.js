25f3b2d59b8294d9d132518e1528bf2d9299b6a2
ltfschoen <ltfschoen@gmail.com>
2014-09-21 07:39:48 +1000

    Converted sites to AngularJS ngRepeat

--- app/assets/javascripts/ng-app/controllers/controllers.js
+++ app/assets/javascripts/ng-app/controllers/controllers.js
@@ -7,6 +7,15 @@
   dragsterControllers.controller('HomeCtrl', ['$scope', 'Websites', function($scope, Websites) {
     $scope.websites = Websites.query();
     console.log($scope.websites);
+
+    $scope.enableHood = function(item){
+      item.hooded = true;
+    };
+
+    $scope.disableEdit = function(item){
+      item.unhooded = false;
+    };
+
   }]);
 
 })();
